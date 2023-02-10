faca8ff9285b584a205923d2c4c50f161dae247c
Leyla Sadirkhanly <slsedrik@gmail.com>
2023-02-10 20:34:38 +0400

    first commit

---
+++ 10 Feb.js
@@ -0,0 +1,9 @@
+let buttonStatus = ""
+if (buttonStatus == "enabled") {
+    console.log("Execute clickBtn()");
+} else if (buttonStatus == "disabled") {
+    console.log("Execute fillForm()");
+} else {
+    console.log("Button has another status")
+}
+console.log("Continue...")
