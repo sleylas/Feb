f45d491ead8c9e7f3a52cd8cb6de527d65f751e4
Leyla Sadirkhanly <slsedrik@gmail.com>
2023-02-10 20:40:28 +0400

    addded

---
+++ HW_7.1.js
@@ -0,0 +1,8 @@
+let postalLocker = [null, null, null, "4 - ваша посылка", null];
+let cell = 4;
+let balance = 100;
+let smsCode = "A001DFX0";
+
+if (postalLocker[cell - 1] && smsCode === "A001DFX0") {
+    console.log(`Вы получили посылку из ячейки ${cell}, и ваш счет составляет:... p`);
+}
